d84b2f5e381249e6baf28f240aa4360b34642efb
xz3t <leu.valentin@gmail.com>
2020-12-05 11:39:58 +0000

    Hide SECRET_KEY

--- apini_md/settings.py
+++ apini_md/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1xxpkbff)b96&sjboyaa24m1mhj!m=%7*lfj9&y-p9jfy%ml3g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
