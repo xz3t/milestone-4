be4859e1454b8db5c8b59ece7492fa5e44006c42
xz3t <leu.valentin@gmail.com>
2020-12-14 16:21:26 +0000

    Installed crispy-forms

--- apini_md/settings.py
+++ apini_md/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'events',
     'bag',
     'checkout',
+
+    #Other
+    'crispy-forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'apini_md.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
