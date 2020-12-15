d32404e16151d0f5c6fc7d02a1ed9feab7d3fd83
xz3t <leu.valentin@gmail.com>
2020-12-15 16:04:06 +0000

    Added core checkout functionality with Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,9 @@ from django.conf import settings
 from django.utils.translation import ugettext as _
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -15,12 +18,25 @@ def checkout(request):
         messages.error(request, _("There's nothing in your bag at the moment"))
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, _('Stripe public key is missing.'))
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test client secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
