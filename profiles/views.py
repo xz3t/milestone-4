9ac5f0ff079caed9e114b670fcd2d81e72f80ebb
xz3t <leu.valentin@gmail.com>
2020-12-17 16:59:09 +0000

    Added order history view, urls and updated profile template

--- profiles/views.py
+++ profiles/views.py
@@ -4,13 +4,14 @@ from django.utils.translation import ugettext as _
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    """ If method is post save profile information """
+    """ If method is post, save profile information """
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
@@ -25,6 +26,24 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ Retrieve order and retur"""
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        _(f'This is a past confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date.')
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
