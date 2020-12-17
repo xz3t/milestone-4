7c2db40fe0c774cc62cc7a782e51ee72a9e4b6cf
xz3t <leu.valentin@gmail.com>
2020-12-17 16:26:57 +0000

    Added profile form,views and updated and styled template

--- profiles/models.py
+++ profiles/models.py
@@ -18,7 +18,7 @@ class UserProfile(models.Model):
     default_phone_number = models.CharField(
         max_length=20, null=True, blank=True)
     default_country = CountryField(
-        blank_label='Country *', null=True, blank=True)
+        blank_label='Country', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(
         max_length=40, null=True, blank=True)
