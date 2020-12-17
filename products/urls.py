be13d02ab47c60398fa4e8a271d5a45cd778a58b
xz3t <leu.valentin@gmail.com>
2020-12-17 21:21:26 +0000

    Added - add_product view,urls and template for superusers

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
     path('addreview/<int:product_id>/', views.add_review, name="add_review"),
-    path('editreview/<int:product_id>/<int:review_id>/', views.edit_review, name="edit_review"),
-    path('deletereview/<int:product_id>/<int:review_id>/', views.delete_review, name="delete_review"),
+    path('editreview/<int:product_id>/<int:review_id>/',
+            views.edit_review, name="edit_review"),
+    path('deletereview/<int:product_id>/<int:review_id>/',
+            views.delete_review, name="delete_review"),
 ]
--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
-from .forms import ReviewForm
+from .forms import ReviewForm, ProductForm
 
 from .models import Product, Category, Review
 
@@ -74,6 +74,17 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
 
 
 def add_review(request, product_id):
