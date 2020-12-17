b50c59a5ba2f58f76c4290b08e38686d0e50663f
xz3t <leu.valentin@gmail.com>
2020-12-17 21:03:31 +0000

    Added product forms for superusers

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Review
+from .models import Review, Product, Category
 
 
 class ReviewForm(forms.ModelForm):
@@ -8,3 +8,19 @@ class ReviewForm(forms.ModelForm):
 
         model = Review
         fields = ("comment",)
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = ''
