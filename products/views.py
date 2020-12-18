0f03a97d3e2dc640270e223b8f94d61359d05c40
xz3t <leu.valentin@gmail.com>
2020-12-18 13:52:31 +0000

    Updated add_product view

--- products/views.py
+++ products/views.py
@@ -78,7 +78,20 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(
+                request,
+                'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -103,7 +116,9 @@ def add_review(request, product_id):
             messages.success(request, 'Review submitted!')
             return redirect('product_detail', product_id)
         else:
-            messages.error(request, 'Failed to submit review. Please ensure the form is valid.')
+            messages.error(
+                request,
+                'Failed to submit review. Please ensure the form is valid.')
     else:
         form = ReviewForm()
         template = 'products/product_detail.html'
