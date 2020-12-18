60297fd3250c8aad20b9f13dc915f6c94f933b63
xz3t <leu.valentin@gmail.com>
2020-12-18 16:38:33 +0000

    Fix image field +  customize widget for edit_product page

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Review, Product, Category
+from .widgets import CustomClearableFileInput
 
 
 class ReviewForm(forms.ModelForm):
@@ -15,6 +16,8 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
