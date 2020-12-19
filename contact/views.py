d98f2dfa63de5234ba980634bb1ce73cf873eedd
xz3t <leu.valentin@gmail.com>
2020-12-19 11:36:06 +0000

    Added contact app views,urls, form and template

---
+++ contact/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from .forms import ContactForm
+from django.contrib import messages
+from django.utils.translation import ugettext as _
+
+# Create your views here.
+
+def contact(request):
+    """ A view to show contact form """
+
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            send_mail(
+                subject,
+                message,
+                from_email,
+                settings.DEFAULT_FROM_EMAIL,
+            )
+            messages.success(request, _('Success! Thank you for your message.'))
+
+    return render(request, 'contact/contact.html', {'form': form})
