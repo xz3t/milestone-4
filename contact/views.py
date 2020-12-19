522152a2f53b8ff1dd0bc6e3425b75a394a13e72
xz3t <leu.valentin@gmail.com>
2020-12-19 23:54:25 +0000

    Fix error in contact send_email

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-from .forms import ContactForm
+from django.conf import settings
 from django.contrib import messages
 from django.utils.translation import ugettext as _
+
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -21,7 +23,7 @@ def contact(request):
                 subject,
                 message,
                 from_email,
-                settings.DEFAULT_FROM_EMAIL,
+                [settings.DEFAULT_FROM_EMAIL,],
             )
             messages.success(request, _('Success! Thank you for your message.'))
 
