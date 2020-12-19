1ebf32d26e97355aaad59ba841a9c70c971f68c0
xz3t <leu.valentin@gmail.com>
2020-12-19 22:27:13 +0000

    Set debug to check if deployed to false

--- apini_md/settings.py
+++ apini_md/settings.py
@@ -27,7 +27,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['apinimd.herokuapp.com', 'localhost']
 
