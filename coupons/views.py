514c30baff4c57f452cc48b2b24e7270996a0129
xz3t <leu.valentin@gmail.com>
2020-12-19 17:53:49 +0000

    Added messages for coupon addition

--- coupons/views.py
+++ coupons/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect
 from django.utils import timezone
 from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.utils.translation import ugettext as _
 from .models import Coupon
 from .forms import CouponApplyForm
 
@@ -17,6 +19,8 @@ def coupon_apply(request):
                                     valid_to__gte=now,
                                     active=True)
             request.session['coupon_id'] = coupon.id
+            messages.success(request, _('Coupon applied!'))
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
+            messages.error(request, _('Invalid or expired coupon!'))
     return redirect('view_bag')
