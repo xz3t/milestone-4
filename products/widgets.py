22a73e5b18fadc486a9b1f99eb528e8b96b24d14
xz3t <leu.valentin@gmail.com>
2021-01-05 19:41:26 +0000

    Run Makemessages to forward files to a translator

--- products/widgets.py
+++ products/widgets.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
-    input_text = _('')
+    input_text = ''
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
