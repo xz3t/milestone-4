ebd6e3f469862f50c1844d0e0ff992ea5c327a47
xz3t <leu.valentin@gmail.com>
2021-01-09 12:54:24 +0000

    Added missing translation for contact page

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.utils.translation import ugettext as _
+
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    email = forms.EmailField(label=_('Email '), required=True)
+    subject = forms.CharField(label=_('Subject '), required=True)
+    message = forms.CharField(
+        label=_('Message '), widget=forms.Textarea, required=True)
